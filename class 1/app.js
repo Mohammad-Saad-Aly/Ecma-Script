34fc111709c38d6bd0559d665af9d7b9a9239495
mohammadsaadali786.92@gmail.com <hammadahmed8787@gmail.com>
2023-03-05 17:06:52 +0500

    class 1

---
+++ class 1/app.js
@@ -0,0 +1,172 @@
+// var let and const
+
+// var fname = 'Saad'
+// var fname = 'ali'
+// console.log(fname)
+
+// let fname = 'Saad'
+// fname = 'Ali'
+// console.log(fname)
+
+// const name = 'Saad'
+// name = 'Ali'
+// console.log(name)
+
+// const obj = {
+//     fname : 'Saad',
+//     age :23
+// }
+// obj.fname = 'Ali'
+// console.log(obj.fname)
+
+// if(true){
+//     const a = 'Saad'
+// }
+// console.log(a)
+
+// template literals 
+
+// let a = 'Hello world'
+// document.write(`Maine likha variable ${a} kuch or likho`)
+// document.write('maine likha ',a)
+
+// Destructuring 
+// object , array
+// var arr = ['Saad','Bilal','Hammad','Ahsan']
+// var getIndex = arr[1] 
+// console.log(getIndex)
+
+// let [name, ,thirdname,fouthname] = arr
+// console.log(thirdname) 
+
+// object destructing
+
+// let obj = {
+//     fname : 'Saad',
+//     age : 23,
+//     roll : 25
+// }
+
+// let {fname, age, roll} = obj
+// console.log(age)
+
+// var obj1 = obj.fname
+// console.log(obj1)
+
+// let arr = [
+//     ['Saad'],
+//     ['Ali',23],
+//     ['Hamza']
+// ]
+
+// let [a,b,c] = arr
+// let [h,n] = b
+// console.log(h)
+// let [a,b] = arr[1]
+
+// console.log(c)
+
+// default , spread
+
+// function sum(a=2,b=10,c=6){
+//     return a + b -c
+// }
+// let a = sum(2,2,2)
+// console.log(a)
+
+// let obj = {
+//     name : 'Saad',
+//     age: 23,
+//     roll : 26
+// }
+// let obj2 = {
+//     ...obj,
+//     fname : 'Hamza'
+// }
+// console.log(obj2)
+
+
+// arrow functions
+
+// let sum = (a,b)=>{return a}
+// let a = sum('hello world')
+// document.write(a)
+
+// let func = ()=>{
+//     console.log('hello world')
+// }
+// func()
+
+
+// function name(){
+//     console.log('hello world')
+// }
+// name()
+
+a
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
